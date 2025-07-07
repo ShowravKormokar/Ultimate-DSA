b00c0be47e2aa5127926ab6429b6037d01727fa4
ShowravKormokar <showravkormokar354@gmail.com>
2025-07-07 23:46:17 +0600

    modify git ignore file

--- temp.cpp
+++ temp.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
-    cout << "This is test program." << endl;
+    int n = 3;
+    int s[n];
+    int i = 0;
+    s[i++] = 3;
+
+    cout << s[i-1];
+    i--;
+    cout << s[i-1];
 
     return 0;
 }
