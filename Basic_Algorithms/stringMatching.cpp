4befa673d9f6a0ca3edf12676a00d4b8ec2ed5fd
ShowravKormokar <showravkormokar354@gmail.com>
2025-07-20 00:06:52 +0600

    Matching 2 strings

--- Basic_Algorithms/stringMatching.cpp
+++ Basic_Algorithms/stringMatching.cpp
@@ -4,23 +4,29 @@ using namespace std;
 int main()
 {
     string str1 = "Showrav";
-    string str2 = "Showrav";
+    string str2 = "Showrav ";
 
     // Most shortcut method
-    bool flag = (str1 == str2); // compare every point in a string
-    cout << flag << endl;       // 1
+    // bool flag = (str1 == str2); // compare every point in a string
+    // cout << flag << endl;       // 1
 
     // Another method
-    // bool flag=false;
-    // while(str1.size() == str2.size() && !str.end())
-    // {
-    //     if (str1[i] == str2[i]) // Ignore upper/lower case
-    //     {
-    //         flag = false;
-    //         break;
-    //     }
-    // }
-    // cout << flag; // 0, because str2 end position have space so string didn't match
+    bool flag = false;
+    int i = 0;
+    while (str1.size() == str2.size() && str1[i] != '\0' && str2[i] != '\0')
+    {
+        if (str1[i] == str2[i])
+        {
+            flag = true;
+        }
+        else
+        {
+            flag = false;
+            break;
+        }
+        i++;
+    }
+    cout << flag; // 0, because str2 end position have space so string didn't match
 
     return 0;
 }
